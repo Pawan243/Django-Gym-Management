e9bf1b0e2cf8dffc23b44e906fcfd2b40d3939bb
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-04 23:21:31 +0530

    modified urls.py

--- GMS/GMS/urls.py
+++ GMS/GMS/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gym.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Home, name='home'),
+    path('about/',About, name = 'about'),
+    path('contact/',Contact, name = 'contact'),
 ]
+ 
