2cce98b0f469aafa6bde2d5e1b46a20a0c290616
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-05 19:31:14 +0530

    modified views.py

--- GMS/gym/views.py
+++ GMS/gym/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout,login
+
 
 # Create your views here.
 def Home(request):
-    return render(request, 'login.html')
-
+    if not request.user.is_staff:
+        return redirect('login')
+    return render(request, 'index.html')
 
 def About(request):
     return render(request, 'about.html')
 
 def Contact(request):
     return render(request, 'contact.html')
+
+def Login(request):
+    error = ""
+    if request.method=='POST':
+        u = request.POST['uname']
+        p = request.POST['pwd']
+
+        user = authenticate(username=u, password=p)
+        try:
+            if user.is_staff:
+                login(request, user)
+                error="no"
+            else:
+                error="yes"
+        except:
+            error="yes"
+    d = {'error':error}
+    return render(request, 'login.html',d)
+
+def Logout(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    logout(request)
+    return redirect('login')
