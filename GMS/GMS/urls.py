ec49b67ff31d3e7b3d9c50a6c8d9204a97b3b9a1
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-07 15:21:57 +0530

    modified urls.py

--- GMS/GMS/urls.py
+++ GMS/GMS/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('about/',About, name = 'about'),
     path('contact/',Contact, name = 'contact'),
     path('admin_login',Login, name='login'),
-    path('logout',Logout, name='logout')
+    path('logout',Logout, name='logout'),
+    path('add_enquiry/',Add_Enquiry,name='add_enquiry'),
+    path('view_enquiry/',View_Enquiry,name='view_enquiry'),
 ]
  
