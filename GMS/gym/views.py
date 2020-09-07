8d9925d2110adb3c6960a8d1e85c2355555ed318
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-07 15:24:26 +0530

    modified views.py

--- GMS/gym/views.py
+++ GMS/gym/views.py
@@ -2,24 +2,29 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, logout,login
-
+from django.contrib.auth import authenticate, logout, login
+from .models import *
 
 # Create your views here.
+
+
 def Home(request):
     if not request.user.is_staff:
         return redirect('login')
     return render(request, 'index.html')
 
+
 def About(request):
     return render(request, 'about.html')
+
 
 def Contact(request):
     return render(request, 'contact.html')
 
+
 def Login(request):
     error = ""
-    if request.method=='POST':
+    if request.method == 'POST':
         u = request.POST['uname']
         p = request.POST['pwd']
 
@@ -27,16 +32,43 @@ def Login(request):
         try:
             if user.is_staff:
                 login(request, user)
-                error="no"
+                error = "no"
             else:
-                error="yes"
+                error = "yes"
         except:
-            error="yes"
-    d = {'error':error}
-    return render(request, 'login.html',d)
+            error = "yes"
+    d = {'error': error}
+    return render(request, 'login.html', d)
+
 
 def Logout(request):
     if not request.user.is_staff:
         return redirect('login')
     logout(request)
     return redirect('login')
+
+
+def Add_Enquiry(request):
+    error = ""
+    if not request.user.is_staff:
+        return redirect('login')
+    if request.method == 'POST':
+        n = request.POST['name']
+        c = request.POST['contact']
+        e = request.POST['emailid']
+        a = request.POST['age']
+        g = request.POST['gender']
+        try:
+            Enquiry.objects.create(
+                name=n, contact=c, emailid=e, age=a, gender=g)
+            error = "no"
+        except:
+            error = "yes"
+    d = {'error': error}
+    return render(request, 'add_enquiry.html', d)
+
+
+def View_Enquiry(request):
+    enq = Enquiry.objects.all()
+    d = {'enq': enq}
+    return render(request, 'view_enquiry.html', d)
