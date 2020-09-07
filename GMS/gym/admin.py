7f12ec299423a3f5af632f1af87c21c949752274
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-07 15:24:05 +0530

    modified admin.py

--- GMS/gym/admin.py
+++ GMS/gym/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Plan,Enquiry, Equipment, Member
 
 # Register your models here.
+
+admin.site.register(Plan)
+admin.site.register(Enquiry)
+admin.site.register(Equipment)
+admin.site.register(Member)
