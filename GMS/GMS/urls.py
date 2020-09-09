61ee18535fd941d60bfad3d0b0ce60f3781d90f9
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-09 23:49:17 +0530

    modified

--- GMS/GMS/urls.py
+++ GMS/GMS/urls.py
@@ -24,11 +24,17 @@ urlpatterns = [
     path('contact/',Contact, name = 'contact'),
     path('admin_login',Login, name='login'),
     path('logout',Logout, name='logout'),
+
     path('add_enquiry/',Add_Enquiry,name='add_enquiry'),
     path('view_enquiry/',View_Enquiry,name='view_enquiry'),
     path('delete_enquiry(?p<int:pid>)', Delete_Enquiry, name='delete_enquiry'),
+
     path('add_equipment/',Add_Equipment,name='add_equipment'),
     path('view_equipment/',View_Equipment,name='view_equipment'),
     path('delete_equipment(?p<int:pid>)', Delete_Equipment, name='delete_equipment'),
+
+    path('add_plan/',Add_Plan,name='add_plan'),
+    path('view_plan/',View_Plan,name='view_plan'),
+    path('delete_plan(?p<int:pid>)', Delete_Plan, name='delete_plan'),
 ]
  
